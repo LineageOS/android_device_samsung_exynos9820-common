7bb518d8d09aaa1c5895d60838525450a559bb1f
GuaiYiHu <guaiyihu@foxmail.com>
2018-09-28 13:52:17 +0800

    exynos9820-common: Enable WIFI_HIDL_FEATURE_DUAL_INTERFACE
    See https://source.android.com/devices/tech/connect/wifi-sta-ap-concurrency
    
    Working after renaming AP interface from swlan0 to wlan1 in kernel
    
    Change-Id: I64f9e7b76e6dffaf59cd78eae4ba3e75f3b13808

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -171,6 +171,7 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
 # hardware/broadcom/wlan/bcmdhd/config/Android.mk
