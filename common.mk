72c405c3b2071e57e7bf4dcf5149608911c3ed81
Arvind Kumar <karvin@codeaurora.org>
2020-01-09 12:08:57 +0530

    exynos9820-common: Enable graphics mapper version 2.1
     Enable graphics mapper version 2.1
    
    Change-Id: Iad70754a980eadcfd68752c76e569b503d398fae

--- common.mk
+++ common.mk
@@ -67,7 +67,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.2-service \
-    android.hardware.graphics.mapper@2.0-impl
+    android.hardware.graphics.mapper@2.0-impl-2.1
 
 # Doze
 PRODUCT_PACKAGES += \
