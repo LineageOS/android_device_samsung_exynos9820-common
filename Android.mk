26ac630e0226cc91835a00a2729ed17444e5e1b1
Jan Altensen <info@stricted.net>
2020-05-18 12:28:10 +0200

    exynos9820-common: generate vulkan and opencl symlinks
    [Linux4: Drop our old way of doing so]
    
    Change-Id: I25501e0ffcb86b8e21ee44c648b9f14bbb931b8b

--- Android.mk
+++ Android.mk
@@ -18,4 +18,27 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter beyond0lte beyond1lte beyond2lte beyondx, $(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+include $(COMMON_PATH)/AndroidBoard.mk
+
+include $(CLEAR_VARS)
+
+EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.universal9820.so
+
+EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
+$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 32 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so $@
+
+EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(EGL_LIBS))
+$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 64 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
+
 endif
