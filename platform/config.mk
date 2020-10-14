8e4e3bda84d2131ef2184cb5e4f01e9890076a19
Tim Zimmermann <tim@linux4.de>
2020-10-14 14:45:27 +0200

    config: use audio_policy_volumes_sec.xml
    Change-Id: I7b1876de05236600d610ed922631c8eb22690221

--- platform/config.mk
+++ platform/config.mk
@@ -1,6 +1,7 @@
 ### AUDIO
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+    $(PLATFORM_PATH)/config/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(PLATFORM_PATH)/config/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_configuration.xml \
