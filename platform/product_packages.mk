db39f53b23de166243f7c9102bf1bfbc2bc2ad2f
Tim Zimmermann <tim@linux4.de>
2021-04-22 21:36:41 +0200

    exynos9820-common: build libbt-vendor from source
    Change-Id: Id2c01752b590f46a4f91cae14f96b29811bc3baf

--- platform/product_packages.mk
+++ platform/product_packages.mk
@@ -18,7 +18,8 @@ PRODUCT_PACKAGES += \
 ### BLUETOOTH
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl:64 \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor:64
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
