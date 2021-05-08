36c6a0b92ab0ff388e98586869e2ff98f55a69b0
Tim Zimmermann <tim@linux4.de>
2021-05-08 20:47:12 +0200

    exynos9820-common: move init.vendor.rilcommon.rc to devicetree
    * So we can edit it for beyondx
    
    Change-Id: I3ec5053ede1cbbbb00f986b2dd6c4aca2d10012b

--- config/init/Android.mk
+++ config/init/Android.mk
@@ -51,3 +51,10 @@ LOCAL_SRC_FILES := init.exynos9820.usb.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.vendor.rilcommon.rc
+LOCAL_SRC_FILES := init.vendor.rilcommon.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
--- platform/init.mk
+++ platform/init.mk
@@ -7,5 +7,6 @@ PRODUCT_PACKAGES += \
     init.exynos9820.root.rc \
     init.exynos9820.rc \
     init.exynos9820.usb.rc \
+    init.vendor.rilcommon.rc \
     fstab.exynos9820 \
     ueventd.exynos9820.rc
