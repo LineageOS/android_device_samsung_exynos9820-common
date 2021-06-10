70e7f1b0d0882d478b8d8b3d324c07c464969dd0
Tim Zimmermann <tim@linux4.de>
2021-06-10 13:49:36 +0200

    exynos9820-common: cleanup properties
    Change-Id: I47e6ad50462ff8dc87097104d84f4de3a1f355b6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -106,7 +106,6 @@ TARGET_SOC := exynos9820
 
 ## Properties
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
-TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 ## Recovery
