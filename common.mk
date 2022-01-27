70bbef9f63fbf38d154df587ea312ce668b03967
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    exynos9820-common: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- common.mk
+++ common.mk
@@ -59,8 +59,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5-service_64.exynos9820 \
-    libsensorndkbridge \
-    Snap
+    libsensorndkbridge
 
 # ConfigStore
 PRODUCT_PACKAGES += \
