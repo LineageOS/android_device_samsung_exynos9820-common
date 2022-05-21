fc3038963a0d5a3ff5bd50c640d61ef8dd9a6592
Tim Zimmermann <tim@linux4.de>
2022-05-21 07:48:16 +0200

    exynos9820-common: Squashed revert of firmware assertions
    * This isn't a hard requirement actually, really broken firmwares can't even boot recovery anyway
    
    This reverts commit 63532e44e1f38d2a59f5eb972048bd4ee825e049.
    This reverts commit 5c83c3c69b43e0f3461a7ad282bc9aaa0faf1eb0.
    This reverts commit b030e9538884e401bda765ea697eee1a52f49b78.
    
    Change-Id: I4c407779eedc9d59890f6304d3a8f3db84a9d3e7

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -17,16 +17,8 @@
 import common
 import re
 
-def FullOTA_Assertions(info):
-  OTA_Assertions(info)
-  return
-
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-  return
-
-def IncrementalOTA_Assertions(info):
-  OTA_Assertions(info)
   return
 
 def IncrementalOTA_InstallEnd(info):
@@ -38,15 +30,6 @@ def AddImage(info, basename, dest):
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
-
-def OTA_Assertions(info):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-bootloader-min\s*=\s*(\S+)', android_info)
-  if m:
-    bootloader_version = m.group(1)
-    cmd = ('assert(exynos9820.verify_bootloader_min("{}") == "1" || abort("ERROR: This package requires Android 12 based firmware. Please upgrade firmware and retry!"););').format(bootloader_version)
-    info.script.AppendExtra(cmd)
-  return
 
 def OTA_InstallEnd(info):
   AddImage(info, "dtb.img", "/dev/block/by-name/dtb")
