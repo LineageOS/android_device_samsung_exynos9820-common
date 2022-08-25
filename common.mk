2d71bbc2b97658eb657b078c0522f7b86b0c3db4
Tim Zimmermann <tim@linux4.de>
2022-08-25 10:07:10 +0200

    exynos9820-common: Switch to AIDL sensors HAL
    Change-Id: I909510578145057be69041c71836e0a3b436f7b7

--- common.mk
+++ common.mk
@@ -279,7 +279,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.0.vendor:64 \
-    android.hardware.sensors@2.1-service.samsung-multihal
+    android.hardware.sensors-service.samsung-multihal
 
 # Shims
 PRODUCT_PACKAGES += \
