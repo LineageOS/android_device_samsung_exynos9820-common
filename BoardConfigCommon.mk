3639c2fd280eb29ca68fea8725de4b449d25ec76
Tim Zimmermann <tim@linux4.de>
2022-12-01 20:53:26 +0100

    exynos9820-common: Set device model for camera HIDL
    * This was accidentally dropped in a059f306514976d63fabb197097114023916e8ff and never brought back
      since so far it caused no noticeable issues
    
    Change-Id: I9f18e0668b944c6089810ae31fbba6afb84f8a42

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,6 +55,9 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
+## Camera
+$(call soong_config_set,exynos9820CameraVars,exynos9820_model,$(TARGET_DEVICE))
+
 ## DTB
 BOARD_PACK_RADIOIMAGES += dtb.img
 
