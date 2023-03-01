0917d5a3da9d03eabbf3e5dc8d864b969a445ba1
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    exynos9820-common: Build lineage health HAL
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad
    (cherry picked from commit b0ea090330a55bdd69653bcc02aa84e66e56b074)

--- common.mk
+++ common.mk
@@ -159,6 +159,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.samsung
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Livedisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.samsung-exynos
