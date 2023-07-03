94704df1f209937f677f2a81eee7a3a3eca92598
Tim Zimmermann <tim@linux4.de>
2023-07-03 06:35:47 +0200

    exynos9820-common: spen: Use defines for paths and only write mac if it changed
    Change-Id: I6918cc58754a77ba15ab622c22092cdbfdaba218

--- aidl/spen/SPen.h
+++ aidl/spen/SPen.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The LineageOS Project
+ * Copyright (C) 2022-2023 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -13,6 +13,13 @@ namespace vendor {
 namespace samsung {
 namespace hardware {
 namespace spen {
+
+#define SYSFS_CHARGING_NODE "/sys/class/sec/sec_epen/epen_ble_charging_mode"
+#define SPEN_STATE_CHARGE "CHARGE"
+#define SPEN_STATE_NG "NG"
+#define SPEN_ADDR_PATH_VENDOR "/mnt/vendor/efs/spen/blespen_addr"
+#define SPEN_ADDR_PATH "/mnt/vendor/efs/spen/blespen_addr"
+#define SPEN_ADDR_DEFAULT "00:00:00:00:00:00"
 
 class SPen : public BnSPen {
 public:
