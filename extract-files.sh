3aeb20c73858e345b74541cc343132ce0e23b1af
Bruno Martins <bgcngm@gmail.com>
2023-12-05 15:05:18 +0000

    exynos9820-common: Allow extracting only firmware images
    Change-Id: Icab3f013661304e3a9ec89b0816a8139f6b8e8e5

--- extract-files.sh
+++ extract-files.sh
@@ -26,6 +26,7 @@ CLEAN_VENDOR=true
 
 ONLY_COMMON=
 ONLY_TARGET=
+ONLY_FIRMWARE=
 KANG=
 SECTION=
 
@@ -36,6 +37,9 @@ while [ "${#}" -gt 0 ]; do
                 ;;
         --only-target )
                 ONLY_TARGET=true
+                ;;
+        --only-firmware )
+                ONLY_FIRMWARE=true
                 ;;
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
@@ -95,7 +99,9 @@ if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
     setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    if [ -z "${ONLY_FIRMWARE}" ]; then
+        extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    fi
 fi
 
 if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
@@ -103,7 +109,9 @@ if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    if [ -z "${ONLY_FIRMWARE}" ]; then
+        extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    fi
 
     if [ -z "${SECTION}" ] && [ -f "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" ]; then
         extract_firmware "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" "${SRC}"
