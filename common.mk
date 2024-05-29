910d4ef79ba5dbdf3a90617d57a180f7e528c9a4
Tim Zimmermann <tim@linux4.de>
2024-05-29 10:23:48 +0200

    exynos9820-common: Switch to AIDL camera HAL
    Change-Id: Idc9ccc33c4ba9c2c6668d145405da623cf4dfc49

--- common.mk
+++ common.mk
@@ -63,7 +63,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service_64.samsung \
+    android.hardware.camera.provider-service.samsung \
     libsensorndkbridge
 
 PRODUCT_PACKAGES += \
