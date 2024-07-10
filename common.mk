40b30df5424e57836eb5a402d579c74d4c52967a
Tim Zimmermann <tim@linux4.de>
2024-07-10 12:11:54 +0200

    exynos9820-common: Remove implicitly built audio packages
    Change-Id: I7db144ec4844be34b5270527a9c5dbc031d65d66

--- common.mk
+++ common.mk
@@ -32,10 +32,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     audio.primary.exynos9820 \
     audio.r_submix.default \
-    audio.usb.default \
-    libaudioroute \
-    libtinyalsa \
-    libtinycompress
+    audio.usb.default
 
 PRODUCT_PACKAGES += \
     SamsungDAP
