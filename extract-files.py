1e6a7e91f9263a95b3d82d23128da9a99e774451
Tim Zimmermann <tim@linux4.de>
2024-11-09 04:50:39 +0100

    exynos9820-common: Switch to py extract utils
    Change-Id: I992b17986ae224504bfdb37f6ae5fd9e5d2a5d30

---
+++ extract-files.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/samsung/exynos9820-common',
+    'hardware/samsung',
+    'hardware/samsung_slsi-linaro/graphics',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+} # fmt: skip
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/libnfc-nci.conf': blob_fixup()
+        .regex_replace('/data/nfc', '/data/vendor/nfc'),
+    'vendor/lib/liboemcrypto.so': blob_fixup()
+        .add_needed('libshim_oemcrypto.so')
+        .binary_regex_replace(b'fopen', b'kopen'),
+    'vendor/lib64/libsec-ril.so': blob_fixup()
+	.sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
+            '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 08 00 80 D2'),
+    'vendor/lib64/libsensorlistener.so': blob_fixup()
+        .add_needed('libshim_sensorndkbridge.so'),
+    (
+        'vendor/lib64/libkeymaster_helper.so',
+        'vendor/lib64/libskeymaster4device.so',
+    ) : blob_fixup()
+        .replace_needed('libcrypto.so', 'libcrypto-tm.so')
+        .add_needed('libssl-tm.so')
+        .add_needed('libshim_crypto.so'),
+    'vendor/lib/libwvhidl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
+    (
+        'vendor/lib/sensors.grip.so',
+        'vendor/lib64/sensors.grip.so',
+    ) : blob_fixup()
+        .add_needed('libutils-v32.so')
+        .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'exynos9820-common',
+    'samsung',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
