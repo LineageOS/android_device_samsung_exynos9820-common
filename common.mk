8ccc6d7535f83c52eeba136a0292188ebdf66423
Bruno Martins <bgcngm@gmail.com>
2025-06-07 18:38:16 +0100

    exynos9820-common: Switch to Touch AIDL
    Change-Id: Id4fcd7ca97b04f887dec6939419815722097d10c

--- common.mk
+++ common.mk
@@ -297,7 +297,7 @@ PRODUCT_COPY_FILES += \
 
 # Touch HAL
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
+    vendor.lineage.touch-service.samsung
 
 # Update
 AB_OTA_UPDATER := false
