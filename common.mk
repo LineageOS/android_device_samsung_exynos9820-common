b0c8164474edcdff31cf9e36d4ab3a4de1b490e9
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-08-25 13:36:49 +0300

    exynos9820-common: Switch to LiveDisplay AIDL
    Change-Id: I8053e904eaaef4050ba22f07ff89974c18b35116

--- common.mk
+++ common.mk
@@ -148,7 +148,7 @@ PRODUCT_PACKAGES += \
 
 # Livedisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.samsung-exynos
+    vendor.lineage.livedisplay-service.samsung-exynos
 
 # Media
 PRODUCT_COPY_FILES += \
